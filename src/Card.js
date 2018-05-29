75a17df73f84cb97d41672641bc0406bc91b4787
Dennis Sam <denniarems@gmail.com>
2018-05-30 05:11:50 +0530

    Enter

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const Card = ({id,name,email}) => {
+    return (
+      <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+   <img src={`http://robohash.org/${id}?200×200`} alt="robohash"/>
+   <div>
+     <p>{name}</p>
+     <p>{email}</p>
+   </div>
+      </div>
+    );
+  }
+
+
+export default Card;
